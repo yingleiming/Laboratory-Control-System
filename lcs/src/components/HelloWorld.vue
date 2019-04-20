695905171aa30ddeafdeed9a15fdf582f3c9fada
ylm <yingleiming@outlook.com>
2019-04-20 17:48:41 +0800

    改写登陆页面

--- lcs/src/components/HelloWorld.vue
+++ lcs/src/components/HelloWorld.vue
@@ -1,22 +1,26 @@
 <template>
-    <div class="login-box">
-
-        <el-form :model="loginForm" :rules="fieldRules" ref="loginForm" label-position="left" label-width="0px" class="demo-ruleForm login-container">
-          <h3 class="title">系统登录</h3>
-          <el-form-item prop="account">
-            <el-input type="text" v-model="loginForm.account" auto-complete="off" placeholder="账号"></el-input>
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
-          </el-form-item>
-          <!-- <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox> -->
-          <el-form-item style="width:100%;">
-            <el-button type="primary" style="width:48%;" @click.native.prevent="reset">重 置</el-button>
-            <el-button type="primary" style="width:48%;" @click.native.prevent="login" :loading="logining">登 录</el-button>
-          </el-form-item>
-        </el-form>
-
-    </div>
+  <el-container>
+    <el-main>
+      <el-form :model="loginForm" :rules="fieldRules" ref="loginForm" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+        <h3 class="title">系统登录</h3>
+        <el-form-item prop="account">
+          <el-input type="text" v-model="loginForm.account" auto-complete="off" placeholder="请输入用户名">
+            <template slot="prepend">账号</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码">
+            <template slot="prepend">密码</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item style="width:100%;margin-bottom: 0;">
+          <el-button type="primary" style="width:48%;" @click="login('loginForm')">登 录</el-button>
+          <el-button style="width:48%;" @click="reset('loginForm')">重 置</el-button>
+        </el-form-item>
+        <el-button type="text" id="createAccount">创建用户？</el-button>
+      </el-form>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -24,10 +28,9 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      logining: false,
       loginForm: {
-        account: 'admin',
-        password: '123456'
+        account: '',
+        password: ''
       },
       fieldRules: {
         account: [
@@ -41,18 +44,26 @@ export default {
     };
   },
   methods: {
-    login() {
-      let userInfo = {account: this.loginForm.account, password: this.loginForm.password}
-      this.$api.login(JSON.stringify(userInfo)).then((res) => {
-        Cookies.set('token', res.data.token) // 放置token到Cookie
-        sessionStorage.setItem('user', userInfo.account) // 保存用户到本地会话
-        this.$router.push('/')  // 登录成功，跳转到主页
-      }).catch(function (res) {
-        alert(res);
-      });
+    login(loginForm) {
+      this.$refs[loginForm].validate((valid) => {
+        let account = this.loginForm.account;
+        let password = this.loginForm.password;
+        this.$axios.get('/login',{
+          account:account,
+          password:password
+        }).then(
+          (res)=>{
+            console.log(res);
+          }
+        ).catch(
+          (err)=>{
+            console.log(err);
+          }
+        )
+      })
     },
-    reset() {
-      this.$refs.loginForm.resetFields();
+    reset(loginForm) {
+      this.$refs[loginForm].resetFields();
     }
   }
 }
@@ -60,6 +71,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
   .login-container {
     -webkit-border-radius: 5px;
     border-radius: 5px;
--- lcs/src/main.js
+++ lcs/src/main.js
@@ -8,6 +8,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+import axios from 'axios'
+Vue.prototype.$axios= axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
