a14c5bc6c950ad446bef5a3bfdfe2a3a6d553ec5
ylm <yingleiming@outlook.com>
2019-04-24 12:57:39 +0800

    组件开发

--- lcs/src/components/DeviceList.vue
+++ lcs/src/components/DeviceList.vue
@@ -1,6 +1,98 @@
 <template>
     <div>
-      设备列表页
+      <div>
+        <span>查找设备</span>
+        <input type="text" placeholder="请输入设备名称">
+      </div>
+
+      <div class="dev">
+        <div>
+          <span>设备名称：</span>
+          <span>1号设备</span>
+        </div>
+        <div>
+          <span>设备状态：</span>
+          <div>
+            <span>1:正常</span>
+            <span>2:损坏</span>
+          </div>
+        </div>
+        <div>
+          <span>设备ID:</span>
+          <span>464688</span>
+        </div>
+        <div>
+          <span>设备描述：</span>
+          <span>实验室sfsfsf</span>
+        </div>
+        <div>
+          <span>创建时间：</span>
+          <span>2019年3月5日</span>
+        </div>
+        <div>
+          <el-button type="primary" @click="" size="mini">修改此设备</el-button>
+        </div>
+      </div>
+      <div class="dev">
+        <div>
+          <span>设备名称：</span>
+          <span>1号设备</span>
+        </div>
+        <div>
+          <span>设备状态：</span>
+          <div>
+            <span>1:正常</span>
+            <span>2:损坏</span>
+          </div>
+        </div>
+        <div>
+          <span>设备ID:</span>
+          <span>464688</span>
+        </div>
+        <div>
+          <span>设备描述：</span>
+          <span>实验室sfsfsf</span>
+        </div>
+        <div>
+          <span>创建时间：</span>
+          <span>2019年3月5日</span>
+        </div>
+        <div>
+          <el-button type="primary" @click="" size="mini">修改此设备</el-button>
+        </div>
+      </div>
+      <div class="dev">
+        <div>
+          <span>设备名称：</span>
+          <span>1号设备</span>
+        </div>
+        <div>
+          <span>设备状态：</span>
+          <div>
+            <span>1:正常</span>
+            <span>2:损坏</span>
+          </div>
+        </div>
+        <div>
+          <span>设备ID:</span>
+          <span>464688</span>
+        </div>
+        <div>
+          <span>设备描述：</span>
+          <span>实验室sfsfsf</span>
+        </div>
+        <div>
+          <span>创建时间：</span>
+          <span>2019年3月5日</span>
+        </div>
+        <div>
+          <el-button type="primary" @click="" size="mini">修改此设备</el-button>
+        </div>
+      </div>
+
+      <div>
+        <el-button type="success" @click="" size="mini">添加设备</el-button>
+      </div>
     </div>
 </template>
 
@@ -11,5 +103,9 @@
 </script>
 
 <style scoped>
-
+    .dev{
+      border: 1px solid #cccccc;
+      border-radius: 4px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+    }
 </style>
--- lcs/src/components/LaboratoryList.vue
+++ lcs/src/components/LaboratoryList.vue
@@ -1,7 +1,61 @@
 <template>
+  <div>
     <div>
-      实验室列表页
+      <span>查找实验室</span>
+      <input type="text" placeholder="请输入实验室名称">
     </div>
+    <div class="dev">
+      <div>
+        <span>实验室名称：</span>
+        <span>1号实验室</span>
+      </div>
+      <div>
+        <span>当前状态：</span>
+        <div>
+          <span>1:可用</span>
+          <span>0:不可用</span>
+        </div>
+      </div>
+      <div>
+        <el-button type="primary" @click="" size="mini">修改实验室</el-button>
+      </div>
+    </div>
+    <div class="dev">
+      <div>
+        <span>实验室名称：</span>
+        <span>1号实验室</span>
+      </div>
+      <div>
+        <span>当前状态：</span>
+        <div>
+          <span>1:可用</span>
+          <span>0:不可用</span>
+        </div>
+      </div>
+      <div>
+        <el-button type="primary" @click="" size="mini">修改实验室</el-button>
+      </div>
+    </div>
+    <div class="dev">
+      <div>
+        <span>实验室名称：</span>
+        <span>1号实验室</span>
+      </div>
+      <div>
+        <span>当前状态：</span>
+        <div>
+          <span>1:可用</span>
+          <span>0:不可用</span>
+        </div>
+      </div>
+      <div>
+        <el-button type="primary" @click="" size="mini">修改实验室</el-button>
+      </div>
+    </div>
+    <div>
+      <el-button type="success" @click="" size="mini">添加实验室</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +65,9 @@
 </script>
 
 <style scoped>
-
+  .dev{
+    border: 1px solid #cccccc;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+  }
 </style>
